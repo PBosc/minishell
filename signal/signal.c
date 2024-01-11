1ad987fa5065999004884ab14f3ee96d38c52d0a
Pierre Bosc <pibosc@bess-f3r1s12.clusters.42paris.fr>
2024-01-11 02:02:13 +0100

    final this time ?

--- signal/signal.c
+++ signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: pibosc <pibosc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 21:40:45 by pibosc            #+#    #+#             */
-/*   Updated: 2024/01/10 23:41:03 by pibosc           ###   ########.fr       */
+/*   Updated: 2024/01/11 02:01:44 by pibosc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,7 @@ void	sig_handler(int sig)
 	{
 		rl_on_new_line();
 		rl_replace_line("", 0);
-		rl_redisplay();
-	}
-	if (sig == SIGQUIT)
-	{
-		rl_replace_line("", 0);
+		printf("\n");
 		rl_redisplay();
 	}
 }
